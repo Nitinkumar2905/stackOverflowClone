95f90f39f3c92b9b9fda63a7a515cde595ed8408
nitin <nimble2905@gmail.com>
2023-11-29 23:41:05 +0530

    added routes for deleting question with answers and post answer for a question with authenticated user

--- server/routes/question.js
+++ server/routes/question.js
@@ -1,23 +1,22 @@
 const express = require("express")
 const Question = require("../models/Question")
+const QuestionAnswer = require("../models/QuestionAnswer")
 const { body, validationResult } = require("express-validator")
 const router = express.Router()
 const jwt = require("jsonwebtoken")
 const fetchUser = require("../middleware/fetchUser")
-const { findById } = require("../models/User")
 require("dotenv").config()
 
 // Router 1 to create a question
 router.post("/askQuestion", fetchUser, [
     body("QuestionTitle", "Write a suitable title for question").isLength({ min: 1 }),
     body("QuestionDetails", "Explain question in details").isLength({ min: 20 }),
-    body("QuestionTriedMethod", "Tell what methods you have tried to solve this problem").isLength({ min: 20 }),
     body("QuestionTags", "tags are related to the question type").exists()
 ], async (req, res) => {
     try {
         // Destructure property from both model user and body
         const { id: userId, name: userName } = req.user
-        const { QuestionTitle, QuestionDetails, QuestionTriedMethod, QuestionTags } = req.body
+        const { QuestionTitle, QuestionDetails, QuestionTags } = req.body
         // if there are any errors, return bad request and the errors 
         const errors = validationResult(req)
         if (!errors.isEmpty) {
@@ -25,7 +24,7 @@ router.post("/askQuestion", fetchUser, [
         }
 
         const question = new Question({
-            QuestionTitle, QuestionDetails, QuestionTriedMethod, QuestionTags, user: userId,
+            QuestionTitle, QuestionDetails, QuestionTags, user: userId,
             userName: userName
         })
         const savedQuestion = await question.save()
@@ -56,9 +55,9 @@ router.get("/fetchQuestions", async (req, res) => {
 })
 
 // router 3: route to particular question page
-router.get("/:id", async (req, res) => {
-    const questionId = req.params.id
+router.get("/question/:id", async (req, res) => {
     try {
+        const questionId = req.params.id
         const errors = validationResult(req)
         if (!errors.isEmpty) {
             return res.status(400).json({ errors: errors.array() })
@@ -70,8 +69,72 @@ router.get("/:id", async (req, res) => {
         res.json({ question })
     } catch (error) {
         console.error(error);
-        console.log(questionId);
+        // console.log(questionId);
         res.status(500).send("Internal server error")
+    }
+})
+
+// router 4: to answer the question with particular question id
+router.post("/question/answer/:id", [
+    body("answerBody", "Write your answer for this question").exists()
+], fetchUser, async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        const { answerBody } = req.body
+        const questionId = req.params.id
+        const { id: user, name: userName } = req.user
+        const question = await Question.findById(questionId)
+        // if logged in user is same as author of the question
+        if (question.user.toString() === user) {
+            return res.status(400).json("You can't answer the question because you are the author")
+        } else {
+            if (!question) {
+                return res.status(404).json({ errors: "question not found" })
+            }
+            const answer = new QuestionAnswer({
+                answerBody,
+                user, userName, questionId
+            })
+            const savedAnswer = await answer.save()
+            res.json(savedAnswer)
+        }
+    } catch (error) {
+        return res.status(400).json("Internal server error")
+    }
+})
+
+// router 5: to delete a question and if the logged user is same as author
+router.delete("/delete/:id", fetchUser, async (req, res) => {
+    try {
+        const questionId = req.params.id
+        const loggedUserId = req.user.id
+        const errors = validationResult(req)
+        if (!errors.isEmpty) {
+            return res.status(400).json({ error: errors.array() })
+        }
+        const question = await Question.findById(questionId)
+        if (question) {
+            if (question.user.toString() === loggedUserId) {
+                // deleting the question associated with this question id
+                await Question.findByIdAndDelete(questionId)
+                // deleting multiple answer associated with this question id
+                await QuestionAnswer.deleteMany({ questionId })
+
+                res.json({ message: "Question deleted successfully" })
+            }
+            else {
+                return res.status(403).json("cant delete this question as you are not author of this question")
+            }
+        }
+        else {
+            return res.status(404).json("question not found")
+        }
+
+    } catch (error) {
+        console.error(error);
     }
 })
 
