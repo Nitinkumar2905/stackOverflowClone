58c23b4d7f983383195713d965c9e0eb3dbf4570
nitin <nimble2905@gmail.com>
2023-12-12 22:17:41 +0530

    fetched logged user data and displayed

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaPencilAlt } from "react-icons/fa";
 
 const UserProfile = () => {
-  // const [loggedUserDetails, setLoggedUserDetails] = useState([]);
-  // const token = localStorage.getItem("token");
-  // const host = "http://localhost:8000/api/auth";
+  const [loggedUserDetails, setLoggedUserDetails] = useState([]);
+  const token = localStorage.getItem("token");
+  const host = "http://localhost:8000/api/auth";
 
-  // const handleGetUser = async () => {
-  //   const response = await fetch(`${host}/getUser`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "auth-token": token,
-  //     },
-  //   });
-  //   if (response.ok) {
-  //     const data = await response.json();
-  //     console.log(data);
-  //   }
-  // };
+  const handleGetUser = async () => {
+    const response = await fetch(`${host}/getUser`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": token,
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setLoggedUserDetails(data);
+      console.log(typeof loggedUserDetails);
+    }
+  };
+  useEffect(() => {
+    handleGetUser();
+    // eslint-disable-next-line
+  }, [host]);
   return (
     <>
       <div className="flex w-[90%] mx-auto justify-center">
@@ -31,8 +38,25 @@ const UserProfile = () => {
               &lt; Back to Home
             </Link>
           </div>
-          <div className="flex justify-center w-[80%] bg-gray-50">
-            <div className="my-4 w-[96%] border-2">user profile</div>
+          <div className="flex flex-col w-[96%] space-y-10 bg-gray-50">
+            <div className="m-4">
+              <div className="flex w-full justify-between">
+                <div className="flex flex-col justify-between border-2 border-black">
+                  {loggedUserDetails.user && (
+                    <div className="flex flex-col items-start m-4">
+                      <span>{loggedUserDetails.user.name}</span>
+                      <span>{loggedUserDetails.user.email}</span>
+                    </div>
+                  )}
+                  <div>user other details</div>
+                </div>
+                <div className="text-xs flex  cursor-pointer border-[1px] rounded-lg border-gray-600 h-fit w-fit px-2 py-2">
+                  <FaPencilAlt className="mt-[2.5px] mx-2"></FaPencilAlt>
+                  Edit profile
+                </div>
+              </div>
+              <div>2nd box</div>
+            </div>
           </div>
         </div>
       </div>
