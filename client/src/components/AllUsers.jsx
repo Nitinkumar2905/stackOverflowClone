aa1731ee2b6be75478e2b6d31274a55245dd253b
nitin <nimble2905@gmail.com>
2023-12-12 22:11:22 +0530

    fetched data for all users page

---
+++ client/src/components/AllUsers.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import HomeSidebar from "./HomeSiderbar";
+
+const AllUsers = () => {
+  const host = "http://localhost:8000/api/auth";
+  const [usersData, setUsersData] = useState([]);
+  const fetchAllUsers = async () => {
+    const response = await fetch(`${host}/getAllUsers`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setUsersData(data);
+    }
+  };
+  useEffect(() => {
+    fetchAllUsers();
+  }, [host]);
+  return (
+    <div className="space-x-3 flex w-full">
+      {/* <HomeSidebar></HomeSidebar> */}
+      <div className="flex items-start w-fit mx-auto flex-col space-y-3">
+        {usersData &&
+          usersData.map((user, index) => {
+            return (
+              <>
+                <div className="flex flex-col items-start" key={index}>
+                  <div className="flex ">
+                    {user.email}
+                    {user.name}
+                    {user._id}
+                  </div>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
