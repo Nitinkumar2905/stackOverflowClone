cabde78a5ff2b2ce9d17350f29b850024d73a375
nitin <nimble2905@gmail.com>
2023-12-25 23:44:32 +0530

    added static one to for uploads

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const app = express()
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+app.use(express.static("uploads"))
 
 app.get("/", (req, res) => {
     res.json("hello world")
