02806843aeb7264cdbf97ad44970d0636f8d4f76
nitin <nimble2905@gmail.com>
2024-02-28 14:03:48 +0530

    changes frontend host link

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const port = 8000;
 connectToMongo()
 const app = express()
 
-// app.use(express.json({ limit: "30mb", extended: true }))
-// app.use(express.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors(
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
+app.use(
+    cors(
     {
         origin: ["https://stackoverflowclone-frontend.vercel.app"],
         methods: ["POST", "GET", "DELETE", "PUT"],
